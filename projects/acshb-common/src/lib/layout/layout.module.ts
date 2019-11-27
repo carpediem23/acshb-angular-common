31a3051c7ae5595195fd7834c4fa3db3666e666a
Alptuğ Türkiş <alptug.turkis@calisma.local>
2019-11-27 14:54:38 +0300

    commit

--- projects/acshb-common/src/lib/layout/layout.module.ts
+++ projects/acshb-common/src/lib/layout/layout.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { WrapperComponent } from './wrapper/wrapper.component';
 
 @NgModule({
-  declarations: [ToolbarComponent],
+  declarations: [ToolbarComponent, WrapperComponent],
   imports: [
     CommonModule
   ],
--- projects/app/src/app/app.module.ts
+++ projects/app/src/app/app.module.ts
@@ -4,8 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { LayoutModule } from '../../../../dist/acshb-common';
-import { ComponentsModule } from '../../../../dist/acshb-common';
+import { LayoutModule } from 'acshb-common';
+import { ComponentsModule } from 'acshb-common';
 
 @NgModule({
   declarations: [
