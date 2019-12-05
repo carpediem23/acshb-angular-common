35481a81c300408c6c04f0333179849d91d1f485
Alptuğ Türkiş <alptug.turkis@calisma.local>
2019-12-05 14:59:35 +0300

    jsdoc örnek eklendi.

--- projects/acshb-common/src/lib/alert/alert.service.ts
+++ projects/acshb-common/src/lib/alert/alert.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Router, NavigationStart } from '@angular/router';
 import { Observable, Subject } from 'rxjs';
-import { filter } from 'rxjs/operators';
-
 import { Alert, AlertType } from './alert.model';
+
 /**
  *
  *
@@ -15,8 +14,6 @@ import { Alert, AlertType } from './alert.model';
 @Injectable({
     providedIn: 'root'
 })
-
-
 export class AlertService {
     private subject = new Subject<Alert>();
     private keepAfterRouteChange = false;
@@ -35,7 +32,7 @@ export class AlertService {
 	
 	/**
 	 * 
-	 * @param alertId nullable string
+	 * @param {string?} alertId alert kimliği.
 	 * @description alert çağrısı yapıldıkça bu method tetiklenir.
 	 */
     onAlert(alertId?: string): Observable<Alert> {
@@ -47,7 +44,7 @@ export class AlertService {
 	 * @param {string} message ekranda gösterilmek istenilen mesajdır.
      * @param {string?} title ekranda gösterilmek istenilen alert' in başlığıdır.
      * @param {boolean?} timer eğer belli bir süre sonra kaybolması isteniliyorsa kullanılmalıdır. 
-     * @param {string?} alertId aler' e ait olan kimliktir.
+     * @param {string?} alertId alert' e ait olan kimliktir.
      * @return {void}
 	 * @description Ekranda 'başarılı' alert göstermek için çağırılır.
 	 */
@@ -55,23 +52,62 @@ export class AlertService {
         this.alert(new Alert({ message, title, timer, type: AlertType.Success, alertId }));
     }
 
+    /**
+	 * 
+	 * @param {string} message ekranda gösterilmek istenilen mesajdır.
+     * @param {string?} title ekranda gösterilmek istenilen alert' in başlığıdır.
+     * @param {boolean?} timer eğer belli bir süre sonra kaybolması isteniliyorsa kullanılmalıdır. 
+     * @param {string?} alertId alert' e ait olan kimliktir.
+     * @return {void}
+	 * @description Ekranda 'hata' alert göstermek için çağırılır.
+	 */
     error(message: string, title?: string, timer?: boolean, alertId?: string) {
         this.alert(new Alert({ message, title, timer, type: AlertType.Error, alertId }));
     }
 
+    /**
+	 * 
+	 * @param {string} message ekranda gösterilmek istenilen mesajdır.
+     * @param {string?} title ekranda gösterilmek istenilen alert' in başlığıdır.
+     * @param {boolean?} timer eğer belli bir süre sonra kaybolması isteniliyorsa kullanılmalıdır. 
+     * @param {string?} alertId alert' e ait olan kimliktir.
+     * @return {void}
+	 * @description Ekranda 'bilgi' alert göstermek için çağırılır.
+	 */
     info(message: string, title?: string, timer?: boolean, alertId?: string) {
         this.alert(new Alert({ message, title, timer, type: AlertType.Info, alertId }));
     }
 
+    /**
+	 * 
+	 * @param {string} message ekranda gösterilmek istenilen mesajdır.
+     * @param {string?} title ekranda gösterilmek istenilen alert' in başlığıdır.
+     * @param {boolean?} timer eğer belli bir süre sonra kaybolması isteniliyorsa kullanılmalıdır. 
+     * @param {string?} alertId alert' e ait olan kimliktir.
+     * @return {void}
+	 * @description Ekranda 'uyarı' alert göstermek için çağırılır.
+	 */
     warning(message: string, title?: string, timer?: boolean, alertId?: string) {
         this.alert(new Alert({ message, title, timer, type: AlertType.Warning, alertId }));
     }
 
+    /**
+	 * 
+	 * @param {Alert} alert
+     * @return {void}
+	 * @description Servisi alert geldiğinie dair bilgilendirir.
+	 */
     alert(alert: Alert) {
         this.keepAfterRouteChange = alert.keepAfterRouteChange;
         this.subject.next(alert);
     }
 
+    /**
+	 * 
+	 * @param {string?} alertId
+     * @return {void}
+	 * @description İlgili alert' i siler.
+	 */
     clear(alertId?: string) {
         this.subject.next(new Alert({ alertId }));
     }
