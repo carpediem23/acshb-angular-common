91885b440cc312376d9a3b27cb46d14d2892992a
Alptuğ Türkiş <alptug.turkis@calisma.local>
2019-12-25 16:38:12 +0300

    Düzenlendi.

--- projects/acshb-common/src/public-api.ts
+++ projects/acshb-common/src/public-api.ts
@@ -7,6 +7,7 @@ export * from './lib/components/components.module';
 export * from './lib/alert/alert.module';
 export * from './lib/modal/modal.module';
 export * from './lib/form/form.module';
+export * from './lib/datatable/datatable.module';
 export * from './lib/chart/chart.module';
 export * from './lib/alert/services/alert.service';
 export * from './lib/form/models/form.model';
