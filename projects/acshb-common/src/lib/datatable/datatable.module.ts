0aa936e3fab9ea766b0650f7c562224e7c8d7d8a
Alptuğ Türkiş <alptug.turkis@calisma.local>
2020-04-07 22:23:57 +0300

    Düzenlendi.

--- projects/acshb-common/src/lib/datatable/datatable.module.ts
+++ projects/acshb-common/src/lib/datatable/datatable.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DatatableComponent } from './datatable.component';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { Ng2TableModule } from 'ng2-table/ng2-table';
 
 @NgModule({
   declarations: [DatatableComponent],
   imports: [
     CommonModule,
-    PaginationModule.forRoot()
+    PaginationModule.forRoot(),
+    Ng2TableModule
+
   ],
   exports: [DatatableComponent]
 })
