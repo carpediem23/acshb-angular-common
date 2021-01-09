38f481458f885ae5dade807f14d63870d81763bf
Alptuğ TÜRKIŞ <alptug.turkis@ailevecalisma.gov.tr>
2021-01-09 16:32:41 +0300

    .

--- projects/app/src/app/components-page/datatables-view/datatables-view.component.ts
+++ projects/app/src/app/components-page/datatables-view/datatables-view.component.ts
@@ -6,52 +6,6 @@ import { DatatablesViewService } from './datatables-view.service';
   templateUrl: './datatables-view.component.html',
   styleUrls: ['./datatables-view.component.scss']
 })
-export class DatatablesViewComponent implements OnInit {
-  columns;
-  data;
-  config;
+export class DatatablesViewComponent {
 
-  constructor(private service: DatatablesViewService) {
-    this.initialize();
-  }
-
-  initialize() {
-    this.columns = [
-      {
-        name: 'postId',
-        title: 'Post Numarası',
-        sort: false
-      },
-      {
-        name: 'id',
-        title: 'Id',
-        sort: false
-      },
-      {
-        name: 'name',
-        title: 'İsim',
-        filtering: { placeholder: 'İsme göre ara' },
-        sort: false
-      },
-      {
-        name: 'body',
-        title: 'İçerik',
-        filtering: { placeholder: 'İçeriğe göre ara' },
-        sort: 'asc'
-      },
-      {
-        name: 'email',
-        title: 'E-Posta',
-        filtering: { placeholder: 'E-Posta ya göre ara' },
-        sort: 'asc'
-      }
-    ];
-    this.data = [];
-  }
-
-  ngOnInit(): void {
-    this.service.fetch().subscribe((data: any) => {
-      this.data = data;
-    });
-  }
 }
